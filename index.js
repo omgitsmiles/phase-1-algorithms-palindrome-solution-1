57d7940cf2f933a277edb34fc055b18c35ec04d2
+ <+>
2022-08-05 15:08:58 -0400

    commit tree 88b62b160e988b147ce98323ab456d6b9d7df98f

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 function isPalindrome(word) {
   // Write your algorithm here
+ const palindrome = word.split('').reverse().join('')
+ return palindrome === word
 }
 
 /* 
   Add your pseudocode here
+use all the method in one line of code, under one variable
+instead of if statement, use === to return true or false
 */
 
 /*
   Add written explanation of your solution here
+    to optimize code, join all methods at once, remove the redundant declaration of variable to one
+  return that constant to see if matches with the word
+
 */
 
 // You can run `node index.js` to view these console logs
